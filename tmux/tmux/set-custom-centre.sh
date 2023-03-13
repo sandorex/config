de8ac90e4cd4a25afb28c37415e9cc393233e4db
sandorex (wvm) <rzhw3h@gmail.com>
2023-03-13 09:16:47 +0100

    A wvm

--- tmux/tmux/set-custom-centre.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# set-custom-centre.sh - sets custom var for the current pane focused
-
-ID=$(tmux display-message -pF '#{=-1:window_id}')
-tmux set -gq @custom_centre "#{E:@custom_centre_${ID}}"
-
