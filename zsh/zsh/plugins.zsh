71f411e99d0b9010b442a322469daed2dab32a55
sandorex (wvm) <rzhw3h@gmail.com>
2023-03-20 14:29:03 +0100

    A wvm
    Added zsh abbr plugin

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -5,6 +5,9 @@
 # sudo plugin, press escape to sudo a command
 source "${0:A:h}"/plugins/ohmyzsh/plugins/sudo/sudo.plugin.zsh
 
+# fish like abbrev
+source "${0:A:h}"/plugins/zsh-abbr/zsh-abbr.zsh
+
 # THIS HAS TO BE LAST!!
 # highlighting
 source "${0:A:h}"/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
