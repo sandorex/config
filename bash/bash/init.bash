00a8b2f4d77989ec2304ad6ea3ded26ef6e7198f
sandorex (Thorium) <rzhw3h@gmail.com>
2023-04-06 22:46:27 +0200

    A Thorium

--- bash/bash/init.bash
+++ bash/bash/init.bash
@@ -5,24 +5,28 @@
 alias reload-shell='source ~/.bashrc'
 alias reload-bash='source ~/.bashrc'
 
+# shellcheck source=../../shell/shell/init.sh
 \. ~/.shell/init.sh
 
-# recreating zsh rprompt with exit code if its not 0
-__bash_rprompt() {
-    if [ "$code" -ne 0 ]; then
-        tput setaf 1
-        printf "%*s" $COLUMNS "[$code]"
-        tput sgr0
-    fi
+# as bash cant really do right aligned prompt im just printing next line with center alignment
+__center_align_printf() {
+    termwidth="$(tput cols)"
+    padding="$(printf '%0.1s' ={1..500})"
+    printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
 }
 
 # minimal prompt
 __prompt_cmd() {
     code=$?
-    PS1='\[$(tput sc; __bash_rprompt; tput rc; tput setaf 2)\]$\[$(tput sgr0)\] '
+    if [ "$code" -ne 0 ]; then
+        tput setaf 1
+        __center_align_printf "Exited with non-zero code $code"
+        tput sgr0
+    fi
 
     [ -n "$TMUX" ] && echo -en "\033]0;$(pwd)\a"
 }
 
 PROMPT_COMMAND='__prompt_cmd'
+PS1='\[$(tput setaf 2)\]$\[$(tput sgr0)\] '
 
