cbe1d7615fdab2a4ec119d32b56f8fc7ae9dbaef
sandorex (Thorium) <rzhw3h@gmail.com>
2023-04-30 12:08:43 +0200

    A Thorium
    I gon messed up

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -2,6 +2,9 @@
 #
 # plugins.zsh - file where all plugin initialization and configuration resides
 
+# fish like abbreviations
+source "$HOME"/.config/shell/plugins/sha-abbr/sha-abbr.zsh
+
 # sudo plugin, press escape to sudo a command
 source "${0:A:h}"/plugins/ohmyzsh/plugins/sudo/sudo.plugin.zsh
 
