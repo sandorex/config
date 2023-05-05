b752fffc75c01d63acc8d583711e8c35ded71874
sandorex (thorium) <rzhw3h@gmail.com>
2023-05-05 11:32:37 +0200

    A thorium

--- ssh/install.sh
+++ ssh/install.sh
@@ -6,7 +6,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
 source ../config.sh
 
+HOST=$(hostname)
 mkdir -p "$HOME"/.ssh
+mkdir -p "$HOME/.ssh/_$HOST"
+ln -s "_$HOST" "$HOME/.ssh/_$HOST.toolbox"
 
-copy ./config "$HOME"/.ssh/config
+# TODO copy script is untested
+cp ./config "$HOME"/.ssh/config
+#copy ./config "$HOME"/.ssh/config
 
