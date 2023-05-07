34c0ed357bd4a50538cfc1fafc4c23259d26d0ac
sandorex (thorium) <rzhw3h@gmail.com>
2023-05-07 03:56:41 +0200

    A thorium.toolbox

--- shell/shell/init.sh
+++ shell/shell/init.sh
@@ -8,15 +8,13 @@ if command -v tmux &>/dev/null; then
     tmux start-server
 fi
 
-
 # automatically sets toolbox hostname
-if [[ "$container" == "oci" ]] && [[ ! -f "/.init" ]]; then
+if [[ "$(hostname)" == "toolbox" ]]; then
     echo "Setting up toolbox hostname.."
 
     source "$DOTFILES"/.shenv
 
     sudo hostname "$SH_HOSTNAME.toolbox"
     echo "$SH_HOSTNAME.toolbox" | sudo tee /etc/hostname >/dev/null
-    sudo touch /.init
 fi
 
