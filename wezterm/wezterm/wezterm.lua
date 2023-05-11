7915bb8335dfd1d958af87bd65c657abf2e55531
sandorex (thorium) <rzhw3h@gmail.com>
2023-05-11 16:08:21 +0200

    A thorium

--- wezterm/wezterm/wezterm.lua
+++ wezterm/wezterm/wezterm.lua
@@ -3,6 +3,16 @@
 local wezterm = require('wezterm')
 local act = wezterm.action
 
+-- is the current desktop a window manager
+local WM = false
+
+-- detect a tiling window manager
+-- NOTE: to add a new one just check what 'XDG_CURRENT_DESKTOP' env var is
+local CUR_DE = os.getenv('XDG_CURRENT_DESKTOP')
+if CUR_DE == 'qtile' then
+    WM = true
+end
+
 local config = {}
 
 -- makes nicer error messages for config errors
@@ -21,6 +31,8 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     config.font = wezterm.font 'FiraCode NFM'
 end
 
+-- makes the tabbar look more like TUI
+config.use_fancy_tab_bar = false;
 config.hide_mouse_cursor_when_typing = false
 config.hide_tab_bar_if_only_one_tab = true
 
@@ -28,12 +40,9 @@ config.hide_tab_bar_if_only_one_tab = true
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
 
--- there is already enough garbage on the top
-config.tab_bar_at_bottom = true
-
-if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
-    -- remove title bar
-    -- config.window_decorations = "RESIZE"
+-- remove title bar in a tiling window managers
+if WM then
+    config.window_decorations = "RESIZE"
 end
 
 config.keys = {
