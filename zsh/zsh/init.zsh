0641be4a6e4d53562e0e89c0c2ea06bfd6763be6
sandorex (thorium) <rzhw3h@gmail.com>
2023-05-14 20:38:51 +0200

    A thorium.toolbox

--- zsh/zsh/init.zsh
+++ zsh/zsh/init.zsh
@@ -52,16 +52,26 @@ HISTSIZE=SAVEHIST=10000
 setopt extended_history append_history hist_ignore_dups hist_ignore_space
 # TODO save history after every command like in bash
 
-setopt no_beep      # no bell
-setopt no_clobber   # do not overwrite stuff with redirection
-setopt no_match     # error when glob doesnt match anything
-setopt auto_cd      # cd into a dir by typing in the path
-setopt notify       # report when background job finishes
-setopt long_list_jobs # long format for jobs
-setopt no_globdots
+setopt no_beep          # no bell
+setopt no_clobber       # do not overwrite stuff with redirection
+setopt no_match         # error when glob doesnt match anything
+setopt auto_cd          # cd into a dir by typing in the path
+setopt no_notify        # report about background jobs only before prompt
+setopt long_list_jobs   # long format for jobs
+setopt globdots         # match dot files with globs implicitly
 setopt extendedglob
 setopt no_caseglob
-setopt no_banghist  # disable !x history expansion
+setopt no_banghist      # disable !x history expansion
+setopt complete_in_word # complete from both ends of a word
+setopt always_to_end    # move cursor to the end of completed word
+setopt auto_list        # list on first tab if ambiguous completion
+setopt auto_menu
+setopt auto_param_slash # if param is a dir add a trailing slash
+
+# zstyle ':completion:*' file-sort name
+# zstyle ':completion:*' menu select=long
+# zstyle ':completion:*' use-cache on
+# zstyle ':completion:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache"
 
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
