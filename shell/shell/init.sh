e8d03348cc355ebbba1f7407cce1b3f38ccbee9d
sandorex (thorium) <rzhw3h@gmail.com>
2023-07-13 22:31:27 +0200

    A thorium

--- shell/shell/init.sh
+++ shell/shell/init.sh
@@ -3,7 +3,15 @@
 # init.sh - this basically runs apps does not have anything to do with shell
 #           initialization, this file should not be sourced!
 
-
+# NOTE these will run only if in wezterm
 wezterm-set-user-var tab_icon "$WEZTERM_ICON"
 wezterm-set-user-var tab_color "$WEZTERM_COLOR"
+
+if command -v task &>/dev/null; then
+    if task current &>/dev/null; then
+        echo "-- $(task current) --"
+        task status
+        echo "-- Task --"
+    fi
+fi
 
