49db1ac914bf90788c648b4aafc272b451dce883
sandorex (thorium) <rzhw3h@gmail.com>
2023-08-02 13:25:06 +0200

    A thorium

--- shell/shell/init.sh
+++ shell/shell/init.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 #
-# init.sh - this basically runs apps does not have anything to do with shell
-#           initialization, this file should not be sourced!
-
-# NOTE these will run only if in wezterm
-wezterm-set-user-var tab_icon "$WEZTERM_ICON"
-wezterm-set-user-var tab_color "$WEZTERM_COLOR"
+# https://github.com/sandorex/config
+# initialization, ran by shells but not sourced!
 
 if command -v task &>/dev/null; then
     if task_name="$(task current)"; then
--- shell/shell/system.sh
+++ shell/shell/system.sh
@@ -3,6 +3,3 @@
 # system only configuration, not ran in containers
 
 export PROMPT_COLOR=253
-export WEZTERM_ICON="󰯄"
-export WEZTERM_COLOR="white"
-
