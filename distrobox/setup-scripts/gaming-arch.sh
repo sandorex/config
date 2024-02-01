2f48f5dd586fadd4cacc51b6826e46b545b09c9f
sandorex (thorium) <rzhw3h@gmail.com>
2024-02-01 13:26:26 +0100

    A thorium

--- distrobox/setup-scripts/gaming-arch.sh
+++ distrobox/setup-scripts/gaming-arch.sh
@@ -2,11 +2,14 @@
 #
 # https://github.com/sandorex/config
 # arch container for gaming bootstrap
+#
+# container must be in different home as steam saves things in ~/.steam
 
 set -e
 
 IMAGE="docker.io/library/archlinux:latest"
-CONTAINER_NAME="${1:-gaming}"
+ASDF_BRANCH="v0.14.0"
+PROTON_GE_VERSIONS=( latest )
 PACKAGES=(
     vulkan-radeon
     lib32-vulkan-radeon
@@ -24,6 +27,10 @@ PACKAGES=(
     perl
     coreutils
 
+    # for asdf
+    curl
+    git
+
     # editor so i dont have to use vim
     nano
 
@@ -33,7 +40,7 @@ PACKAGES=(
 )
 
 if [[ "$1" == "setup" ]] && [[ -v container ]]; then
-    echo ":: Generating en_US.UTF8 locale (for game compatibility)"
+    echo ":: Generating en_US.UTF8 locale"
     sudo sed -i '/^#en_US.UTF-8 UTF-8.*/s/^#//' /etc/locale.gen
     sudo locale-gen
 
@@ -51,16 +58,46 @@ EOF
 
     echo ":: Installing the packages"
     sudo pacman -S --noconfirm "${PACKAGES[@]}"
+
+    echo ":: Installing asdf version manager"
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch "$ASDF_BRANCH"
+
+    echo ":: Adding asdf to shell init files"
+    cat <<EOF | tee -a ~/.zshrc ~/.bashrc
+# load asdf (added by $0)
+. "$HOME/.asdf/asdf.sh"
+EOF
+
+    echo ":: Installing proton-ge using asdf"
+    asdf plugin add protonge
+
+    for i in "${PROTON_GE_VERSIONS[@]}"; do
+        asdf install protonge "$i"
+    done
+
+    echo ":: Exporting Steam to host"
+    distrobox-export --app Steam
+
+    echo ":: Container setup finished"
 else
     if [[ -v container ]]; then
         echo "Running distrobox inside a container is not recommended"
         exit 1
     fi
 
+    if [[ "$#" -lt 2 ]]; then
+        echo "Usage: $0 <container_name> <container_home>"
+        exit 1
+    fi
+
+    CONTAINER_NAME="$1"
+    CONTAINER_HOME="$2"
+
     echo ":: Creating distrobox container"
     distrobox create --image "$IMAGE" \
                      --name "$CONTAINER_NAME" \
-                     --pre-init-hooks "printf '$(uname -n)' >> /etc/hostname" \
+                     --home "$CONTAINER_HOME" \
+                     --pre-init-hooks "cat /run/host/etc/hostname > /etc/hostname" \
                      "${ARGS[@]}"
 
     echo ":: Starting setup process"
