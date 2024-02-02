467535a3c2a760293f11deaab0af1fbc445968f0
sandorex (thorium) <rzhw3h@gmail.com>
2024-02-02 17:16:57 +0100

    A thorium
    Adding back the submodules, fixing the setup-ansible.sh script

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -1,36 +1,45 @@
 #!/usr/bin/env bash
 #
 # https://github.com/sandorex/config
-# script sets up ansible using python venv, requires python3 and python3-venv
+# script sets up ansible using python venv, each python version has its own
+# venv as it often breaks without errors if there is a mismatch in versions
 
-VENV_DIR="venv"
+export VENV_BASE_DIR="venv"
 
 if ! command -v python3 &>/dev/null; then
     echo "Python3 was not found"
     return 1
 fi
 
+# get version string like 3.11, 3.14, ..
+PYTHON_VERSION="py$(python3 -c 'import sys; print(str(sys.version_info.major) + "." + str(sys.version_info.minor))')"
+
+mkdir -p "$VENV_BASE_DIR"
+
+export VENV_DIR="$VENV_BASE_DIR/$PYTHON_VERSION"
+
+
 # check if venv is already created
 if [[ ! -d "$VENV_DIR" ]]; then
     # create venv
-    if ! python3 -m venv "$VENV_DIR"; then
+    if ! python3 -m venv --prompt "ansible venv" "$VENV_DIR"; then
         echo "Failed creating python virtualenv, is python3-venv installed?"
         return 1
     fi
 fi
 
-if [[ ! -f "$VENV_DIR/bin/activate" ]]; then
-    echo "Activate script was not found in virtaulenv, try deleting './$VENV_DIR'"
-    return 1
-fi
-
 # activate virtualenv
-source "$VENV_DIR/bin/activate"
-
-# install ansible if not present
-if ! command -v ansible &>/dev/null; then
-    pip install ansible
+if ! source "$VENV_DIR/bin/activate"; then
+    echo "Failed to activate virtualenv, try deleteing './$VENV_DIR'"
+    exit 1
 fi
 
+# install ansible-core if not present
+if ! command -v ansible-playbook &>/dev/null; then
+    pip install ansible-core
+fi
+
+echo
 echo "You can exit out of the virtualenv using 'deactivate' command"
+echo
 
