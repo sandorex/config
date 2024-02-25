fdf707389fce1394596aefc3983c8b3868cf2ff6
sandorex (thorium) <rzhw3h@gmail.com>
2024-02-25 08:01:36 +0100

    A thorium

--- roles/autostart/files/null-wayland-clipboard.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# simply set empty string to wayland clipboard as on old versions it causes problems with some applications like micro editor
-
-set -e
-set -o pipefail
-
-printf " " | wl-copy
-
