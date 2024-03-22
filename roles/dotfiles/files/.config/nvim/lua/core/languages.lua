93a19ea194789ff314ffebfe4d148358f2ddfff1
sandorex (thorium) <rzhw3h@gmail.com>
2024-03-22 21:03:22 +0100

    A thorium
    Removed old kde plasma theming
    Improved dev container ansible playbook
    More nice tweaks to neovim configuration

--- roles/dotfiles/files/.config/nvim/lua/core/languages.lua
+++ roles/dotfiles/files/.config/nvim/lua/core/languages.lua
@@ -2,3 +2,18 @@
 
 -- allow comments in JSON
 vim.cmd([[autocmd FileType json syntax match Comment +\/\/.\+$+]])
+
+-- automatically compile latex if possible
+if vim.fn.executable('latexmk') == 1 then
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = {'*.tex'},
+        callback = function()
+            vim.schedule(function()
+                vim.fn.jobstart(
+                    'latexmk -output-format=pdf'
+                )
+            end)
+        end,
+    })
+end
+
