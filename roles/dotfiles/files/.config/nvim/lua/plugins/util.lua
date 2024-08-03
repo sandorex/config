0f83956032700dd34549ad7bcde24d2adde8792f
sandorex <rzhw3h@gmail.com>
2024-08-03 19:42:01 +0200

    nvim: add mini.clue

--- roles/dotfiles/files/.config/nvim/lua/plugins/util.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins/util.lua
@@ -31,7 +31,9 @@ return {
                             }
                         },
                         path_display = telescope_formatter,
-                        sort_lastused = true,
+                        -- TODO does not work same as fzf
+                        --sort_lastused = true,
+                        sort_mru = true,
                         -- ignore_current_buffer = true,
                     }
                 }
@@ -43,13 +45,6 @@ return {
     },
 
     {
-        enabled = false,
-        'folke/which-key.nvim',
-        event = 'VeryLazy',
-        opts = {},
-    },
-
-    {
         -- flatpak only
         enabled = os.getenv("container") == "flatpak",
         'sandorex/neobrew',
@@ -58,6 +53,68 @@ return {
         config = function()
             require("neobrew").init({})
         end
+    },
+
+    -- replacement for which-key
+    {
+        'echasnovski/mini.clue',
+        version = false,
+        config = function()
+            local miniclue = require('mini.clue')
+            miniclue.setup({
+                triggers = {
+                    -- Leader triggers
+                    { mode = 'n', keys = '<Leader>' },
+                    { mode = 'x', keys = '<Leader>' },
+
+                    -- Built-in completion
+                    { mode = 'i', keys = '<C-x>' },
+
+                    -- `g` key
+                    { mode = 'n', keys = 'g' },
+                    { mode = 'x', keys = 'g' },
+
+                    -- Marks
+                    { mode = 'n', keys = "'" },
+                    { mode = 'n', keys = '`' },
+                    { mode = 'x', keys = "'" },
+                    { mode = 'x', keys = '`' },
+
+                    -- Registers
+                    { mode = 'n', keys = '"' },
+                    { mode = 'x', keys = '"' },
+                    { mode = 'i', keys = '<C-r>' },
+                    { mode = 'c', keys = '<C-r>' },
+
+                    -- Window commands
+                    { mode = 'n', keys = '<C-w>' },
+
+                    -- `z` key
+                    { mode = 'n', keys = 'z' },
+                    { mode = 'x', keys = 'z' },
+                },
+
+                clues = {
+                    -- Enhance this by adding descriptions for <Leader> mapping groups
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.marks(),
+                    miniclue.gen_clues.registers(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
+                },
+
+                window = {
+                    -- 1s is too long
+                    delay = 500,
+
+                    config = {
+                        -- make it fit content
+                        width = 'auto',
+                    }
+                },
+            })
+        end,
     },
 }
 
