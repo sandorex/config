9624b2fda3d0dbec96ae6d5ca358a8c5beba0fe0
sandorex <rzhw3h@gmail.com>
2024-10-07 17:16:41 +0200

    Updating emacs config
    Yes i am making an emacs config after spending so many hours on neovim

--- roles/dotfiles/files/.emacs.d/custom.el
+++ roles/dotfiles/files/.emacs.d/custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(leuven-dark)))
+ '(custom-enabled-themes '(leuven-dark))
+ '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#25202a" :foreground "#cfccd2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "CTDB" :family "FiraCode Nerd Font")))))
--- roles/dotfiles/files/.emacs.d/init.el
+++ roles/dotfiles/files/.emacs.d/init.el
@@ -3,6 +3,34 @@
 
 (package-initialize)
 
+; use treesitter rust mode
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
+
+; TODO enable only if rust-analyzer is present
+;(use-package eglot
+;  :ensure t
+;  :defer t
+;  :hook (rust-ts-mode . eglot-ensure))
+
+;; TODO install these automatically
+(setq treesit-language-source-alist
+  '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (rust "https://github.com/tree-sitter/tree-sitter-rust")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
 (tool-bar-mode 0)      ; remove toolbar
 (menu-bar-mode 0)      ; remove menu bar
 (global-display-line-numbers-mode 1)
@@ -14,6 +42,8 @@
 
 (xterm-mouse-mode 1)     ; enable mouse in terminal
 
+(setq-default scroll-margin 4) ; keep x lines above or below cursor visible
+
 (setq-default indent-tabs-mode nil) ; do not enter tabs randomly.. wtf
 
-
+(put 'narrow-to-region 'disabled nil)
