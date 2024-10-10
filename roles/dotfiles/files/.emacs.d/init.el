de58dfce4444a0a59cbf7ee91600d2e6f3f8d2fd
sandorex <rzhw3h@gmail.com>
2024-10-10 10:02:28 +0200

    emacs: more tweaking
    Also added emacs to boxes

--- roles/dotfiles/files/.emacs.d/init.el
+++ roles/dotfiles/files/.emacs.d/init.el
@@ -1,4 +1,9 @@
-(add-to-list 'load-path "~/.emacs.d/config")
+;; Minimal Configuration
+;;
+;; Using: eglot project.el
+
+;; load from user directory whatever it may be
+(add-to-list 'load-path (concat user-emacs-directory "config"))
 
 ;; load machine specific custom file
 (setq custom-file (concat "custom-" (system-name) ".el"))
@@ -7,19 +12,20 @@
 ;; disable the startup message
 (defun display-startup-echo-area-message ())
 
-;; move backup directory
-(setq-default backup-directory-alist '(("~/.emacs.bak")))
+;; set backup in tmp so its available if needed but deleted often for security
+(setq-default backup-directory-alist '(("." . "/tmp/emacs-bak")))
 
 ;; start with appropriate size
 (add-to-list 'default-frame-alist '(height . 30))
 (add-to-list 'default-frame-alist '(width . 90))
 
 (unless (display-graphic-p)
-  (progn
-    ;; fix some keys in terminal (has to be set in terminal explicitly)
-    ;; NOTE keymap-set does not work here
-    (define-key key-translation-map (kbd "\e[[;") (kbd "C-;"))
-    ))
+  ;; fix some keys in terminal (has to be set in terminal explicitly)
+  ;; NOTE keymap-set does not work here
+  (define-key key-translation-map (kbd "\e[[;") (kbd "C-;")))
+
+;; keybindings (currently too short for separate file)
+(keymap-global-set "<f9>" 'recompile)
 
 (eval-after-load 'compile (load "compile-mode.el"))
 (load "editor.el")
