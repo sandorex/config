000d942584fb4c0bd747816ff89a566a3e3fd53b
sandorex <rzhw3h@gmail.com>
2024-12-06 15:41:10 +0100

    boxes: cleaning up fedora-everything

--- boxes/fedora-everything/scripts/opt_utils.sh
+++ boxes/fedora-everything/scripts/opt_utils.sh
@@ -8,3 +8,7 @@ dnf -y install $(</temp/pkglist/utils-dnf)
 npm install -g $(</temp/pkglist/utils-npm)
 pip install $(</temp/pkglist/utils-pip)
 
+wget 'https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz'
+tar -xvf zellij*.tar.gz
+mv zellij /usr/local/bin/zellij
+
