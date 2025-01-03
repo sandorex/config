bc794901f38ed274b3c60a208294f0e54c321551
sandorex <rzhw3h@gmail.com>
2025-01-03 15:02:58 +0100

    shell: fix alias a

--- roles/dotfiles/files/.config/shell/aliases.sh
+++ roles/dotfiles/files/.config/shell/aliases.sh
@@ -11,7 +11,7 @@ if [[ ! -v ZSH_VERSION ]]; then
     compdef() { :; }
 fi
 
-function a() { arcam start "$@"; arcam shell; }
+alias a=arcam
 alias e="$EDITOR1"
 alias ee="$EDITOR2"
 alias eee="$EDITOR3"
