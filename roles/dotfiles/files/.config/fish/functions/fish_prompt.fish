5fd286feee85b9b8ab855426bf4331e42904b283
sandorex <rzhw3h@gmail.com>
2025-01-31 18:57:55 +0100

    fish: prompt feature parity with zsh

--- roles/dotfiles/files/.config/fish/functions/fish_prompt.fish
+++ roles/dotfiles/files/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,14 @@
 function fish_prompt
     set -l _status $status
+
+    # if there are background jobs draw underline    
+    if test (jobs | wc -l) -ne 0
+        echo -n (set_color magenta --underline)
+    else
+        echo -n (set_color magenta)
+    end
     
-    echo -n (set_color magenta)(whoami)(set_color normal)@(set_color blue)(prompt_hostname) (set_color $PROMPT_ICON_COLOR_HEX)$PROMPT_ICON(set_color normal)
+    echo -n (whoami)(set_color normal)@(set_color blue)(prompt_hostname) (set_color $PROMPT_ICON_COLOR_HEX)$PROMPT_ICON(set_color normal)
 
     # color the arrow in red if exit code is not 0
     if test $_status -ne 0
